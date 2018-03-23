1d2d9dd08da796271244c90b249d6cd885cf1fee
Frederick Ofori <FrederickCodes@gmail.com>
2018-03-23 11:05:12 +0000

    Starter files created using - dotnet new console -

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpRestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
